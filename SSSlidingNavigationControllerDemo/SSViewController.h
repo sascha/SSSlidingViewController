c1769dff90d4af3498376d3422c23b97fa6a7e6c
Sascha Schwabbauer <sascha.schwabbauer@me.com>
2013-03-03 20:25:05 +0100

    Initial Commit

---
+++ SSSlidingNavigationControllerDemo/SSViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSViewController.h
+//  SSSlidingNavigationControllerDemo
+//
+//  Created by Sascha Schwabbauer on 03.03.13.
+//  Copyright (c) 2013 Sascha Schwabbauer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSViewController : UIViewController
+
+@end
---
+++ SSSlidingNavigationControllerDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SSSlidingNavigationControllerDemo
+//
+//  Created by Sascha Schwabbauer on 03.03.13.
+//  Copyright (c) 2013 Sascha Schwabbauer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SSAppDelegate class]));
+    }
+}
